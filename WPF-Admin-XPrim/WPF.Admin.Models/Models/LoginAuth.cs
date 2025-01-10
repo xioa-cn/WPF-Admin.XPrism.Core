670bcacfb188e2e5d2cda7e2dae38d08d9b95f99
XIOA <1327916255@qq.com>
2025-01-10 17:31:13 +0800

    router.json

---
+++ WPF-Admin-XPrim/WPF.Admin.Models/Models/LoginAuth.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF.Admin.Models.Models;
+
+public enum LoginAuth
+{
+    None,
+    Admin,//超级权限
+    FUser,//前台用户
+    HUser,//后台用户
+}
---
+++ WPF-Admin-XPrim/WPF.Admin.Models/Models/TreeItemModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WPF.Admin.Models.Models;
+
+public partial class TreeItemModel : BindableBase {
+    /// <summary>
+    /// 持久化使用组件
+    /// </summary>
+    public bool IsPersistence { get; set; } = true;
+
+    public string? Content { get; set; }
+
+    public LoginAuth LoginAuth { get; set; }
+
+    public object? Icon { get; set; }
+
+    public Type? Page { get; set; }
+
+    [ObservableProperty] private bool _isChecked;
+
+    [ObservableProperty] private bool _isExpanded;
+
+    public ObservableCollection<TreeItemModel> Children { get; set; } = new();
+
+    public bool HasChildren => Children.Count > 0;
+
+    public PageCanInterchange PageCanInterchange { get; set; } = PageCanInterchange.Can;
+
+    public TreeItemModel() {
+    }
+
+    [ObservableProperty] private string? _message;
+
+    public bool HasMessage => !string.IsNullOrEmpty(_message);
+
+    public PageStatus PageStatus { get; set; } = PageStatus.Page;
+
+    public TreeItemModel(string con) {
+        this.Content = con;
+    }
+}
